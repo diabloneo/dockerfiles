b9a6242b2dde48514cddae68c97aa6fb3351411c
diabloneo <diabloneo@gmail.com>
2017-07-29 23:09:36 +0800

    Create shadowsocks
    Signed-off-by: diabloneo <diabloneo@gmail.com>

---
+++ shadowsocks/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SS_CONFIG=${SS_CONFIG:-""}
+SS_MODULE=${SS_MODULE:-"ss-server"}
+OBFS_ENABLE=${OBFS_ENABLE:-"false"}
+KCP_CONFIG=${KCP_CONFIG:-""}
+KCP_FLAG=${KCP_FLAG:-"false"}
+
+while getopts "s:m:k:x:o:" OPT; do
+    case $OPT in
+        s)
+            SS_CONFIG=$OPTARG;;
+        m)
+            SS_MODULE=$OPTARG;;
+        o)
+            OBFS_ENABLE=$OPTARG;;
+        k)
+            KCP_CONFIG=$OPTARG;;
+        x)
+            KCP_FLAG="true";;
+    esac
+done
+
+if [ "$KCP_FLAG" == "true" ] && [ "$KCP_CONFIG" != "" ]; then
+    echo -e "\033[32mStarting kcptun......\033[0m"
+    kcptun $KCP_CONFIG 2>&1 &
+else
+    echo -e "\033[33mKcptun not started......\033[0m"
+fi
+
+echo -e "\033[32mStarting shadowsocks......\033[0m"
+if [ "$SS_CONFIG" != "" ]; then
+    if [[ ${OBFS_ENABLE} = "true" ]]; then
+        ${SS_MODULE} ${SS_CONFIG} --plugin obfs-local --plugin-opts "obfs=http;obfs-host=www.bing.com"
+    else
+        $SS_MODULE $SS_CONFIG
+    fi
+else
+    echo -e "\033[31mError: SS_CONFIG is blank!\033[0m"
+    exit 1
+fi
